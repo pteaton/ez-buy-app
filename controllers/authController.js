6be33a6be80caffdd5ac383128d5af04c05b52b0
peaton373 <peaton373@gmail.com>
2020-04-08 12:23:34 -0500

    setup login route, now debugging

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,11 +4,12 @@ const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
 
-
+// route to signup
 router.get('/signup', (req, res) => {
     res.render('auth/signup.ejs')
 })
 
+// specify route to signup
 router.post('/signup', async (req, res, next) => {
     try {
         const createdUsername = req.body.username
@@ -46,6 +47,64 @@ router.post('/signup', async (req, res, next) => {
         next(err)
     }
 })
+
+// route to log in
+router.get('/login', (req, res) => {
+    res.render('auth/login.ejs')
+})
+
+// specify route to log in
+router.post('/login', async (req, res, next) => {
+	try {
+		const user = await User.findOne({ username = req.body.username })
+
+		// if user does not exist
+		if(!user) {
+			req.session.message = "Username or Password is invalid"
+			res.redirect('/auth/login')
+		} 
+			else if (user.password == req.body.password) {
+        		req.session.loggedIn = true
+        		req.session.userId = user.id
+        		req.session.username = user.username
+        		req.session.message = `Welcome back ${user.username}, What do you want to buy?`
+        		res.redirect('/')
+			} 
+	} 	
+	catch(err) {
+		next(err)
+	}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
