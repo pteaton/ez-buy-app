af6b312c9e0dd3704e01ada7c9c3b4697a98a89f
Paresh Sharma <Paresh.Sharma@Valaries-MacBook-Pro.local>
2020-04-08 16:48:07 -0500

    Working on multer

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,39 @@
 const express = require('express')
 const router = express.Router()
 const multer = require('multer')
-const upload = multer({dest: 'uploads/'})
+// const upload = multer({dest: './uploads/'})
+
+
+const storage = multer.diskStorage({
+  destonation: function(req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname)
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+
+  //if the filetype is not right
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true)
+    cb(new Error('File was not saved'))
+  }
+  else {
+
+    cb(null, false)
+  }
+}
+const upload =  multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+})
 
 const Product = require('../models/product')
-
-
 
 // Get home route
 router.get('/', (req, res) => {
