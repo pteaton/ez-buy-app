1753b1e96f56233b431bf2048404205c271de5cd
peaton373 <peaton373@gmail.com>
2020-04-09 11:31:39 -0500

    edited userController.js file

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const Product = require('../models/product')
 const User = require('../models/user')
 
-// GET /users/:userId/dogs
+// GET /users/:userId/products
 router.get('/:userId/products', async (req, res, next) => {
   try {
     const productsToBeSoldByUser = await Product.find({ user: req.params.userId }).populate('user')
