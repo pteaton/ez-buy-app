a92e936d7bf9fc233250ab308e82a529452dc529
Paresh Sharma <Paresh.Sharma@Valaries-MacBook-Pro.local>
2020-04-10 16:10:31 -0500

    user edit route works and now working on user update route

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,23 +22,55 @@ router.get('/:userId/products', async (req, res, next) => {
   }
 })
 
-module.exports = router
+// GET route view account page -- /:id
+router.get('/:userId/viewProfile', async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId)
+    console.log("here is view account page for user")
+    console.log(user)
+    res.render('users/viewProfile.ejs', {
+      user: user,
+      userId: req.session.userId
+    })
 
-GET route edit page
-router.get('/:userId/edit', async (req, res, next) => {
+  } catch(err) {
+    next(err)
+  }
+})
+
+
+
+
+// GET route edit page
+router.get('/:userId/updateProfile', async (req, res, next) => {
   try {
     const foundUser = await User.findById(req.params.id)
-    const currentUser = req.session.userId
-    const session = req.session
 
-    messageToDisplay = req.session.message
-    res.render('user/edit.ejs', {
+    res.render('users/updateProfile.ejs', {
       user: foundUser,
-      currentUser: currentUser,
-      message: messageToDisplay,
-      session: session
+      userId: req.session.useId
     })
   } catch (error) {
     next(error)
   }
 })
+
+router.put('/:userId', async (req, res, next) => {
+  try {
+	const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+  console.log("updatedUser");
+  console.log(updatedUser);
+    res.render('users/viewProfile.ejs', {
+      user: updatedUser,
+      userId: req.session.userId
+    })
+  }
+  catch (err) {
+    next (err)
+  }
+})
+
+
+
+module.exports = router
